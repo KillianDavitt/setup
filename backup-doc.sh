a0e22f0dbb66971f60e87e9b9e570e7c169fd7de
Killian Davitt <davittk@tcd.ie>
2016-08-17 10:51:25 +0100

    Fixed release detection

--- backup-doc.sh
+++ backup-doc.sh
@@ -4,3 +4,4 @@ tar -czvf doc.tar.gz doc
 cat ./.password | gpg --batch --no-tty --yes --passphrase-fd 0 --symmetric -o ./doc-$(date +%d-%m-%y).tar.gz.gpg ./doc.tar.gz
 scp -i ~/.ssh/id_netsoc doc-$(date +%d-%m-%y).tar.gz.gpg killiand@netsoc.tcd.ie:doc-backup/doc-$(date +%d-%m-%y).tar.gz.gpg
 rm doc-$(date +%d-%m-%y).tar.gz*
+rm doc.tar.gz
--- setup.sh
+++ setup.sh
@@ -2,19 +2,15 @@
 
 set -e
 # Are we arch based or debian based?
-release=$(ls /etc/ | grep -release)
-
-if [[ $release == *"Arch"* ]]
-then
-  echo "I think this is arch"
-  get="sudo pacman -Sy"
-  $get git || true
-elif [[ $release == *"debian"* ]]
-then
-  echo "I think this is debian"
-  get="sudo apt-get install -y"
+if [ ! -f /etc/lsb-release ]; then
+    echo "This is not debian, probably arch!"
+    get="sudo pacman -Sy "
+else
+    echo "This is debian based"
+    get="sudo apt-get install -y "
 fi
 
+get git
 echo "Installing git..."
 
 echo "Creating git dir..."
@@ -27,23 +23,9 @@ echo "Linking dotfiles..."
 ln -S ~/git/dotfiles/.vimrc ~/.vimrc
 ln -S ~/git/dotfiles/.bashrc ~/.bashrc
 ln -S ~/git/dotfiles/.zshrc ~/.zshrc
+ln -S ~/git/dotfiles/ssh-config ~/.ssh/config
+
 source ~/.bashrc
-
-echo "Time to set up go!"
-#Setup go first
-ver=go1.5.1.linux-amd64.tar.gz
-echo "Trying to get version: "$ver
-wget https://storage.googleapis.com/golang/$ver
-tar -C /usr/local -xzf $ver
-go get -u github.com/odeke-em/drive/cmd/drive
-drive init ~/drive
-cd ~/drive
-drive pull linux
-
-# Next priority is to setup the go drive cron scripts
-cp ~/git/dotfiles/drivecron/drive_upload.sh /usr/local/bin/
 
 get python3 zsh vim
 
-## Now install private stuff, docs, non-git code, pics
-bash ~/drive/linux/setup.sh
