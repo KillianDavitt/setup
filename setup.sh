d183166af7886e612b141b396a46cd7acbdb8535
Killian Davitt <davittk@tcd.ie>
2016-09-22 18:21:32 +0100

    Changed to debian only, fixed many bugs

--- setup.sh
+++ setup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 set -e
-# Are we arch based or debian based?
-if [ ! -f /etc/lsb-release ]; then
-    echo "This is not debian, probably arch!"
-    get="sudo pacman -Sy "
-else
-    echo "This is debian based"
-    get="sudo apt-get install -y "
-fi
-
-get git
+get="sudo apt-get install -y "
+$get git
 echo "Installing git..."
 
 echo "Creating git dir..."
@@ -20,12 +12,29 @@ echo "Cloning dotfiles..."
 git clone https://github.com/KillianDavitt/dotfiles.git
 
 echo "Linking dotfiles..."
+exit()
+cd ~
 ln -S ~/git/dotfiles/.vimrc ~/.vimrc
 ln -S ~/git/dotfiles/.bashrc ~/.bashrc
 ln -S ~/git/dotfiles/.zshrc ~/.zshrc
+mkdir -p ~/.ssh
+cd ~/.ssh
 ln -S ~/git/dotfiles/ssh-config ~/.ssh/config
 
 source ~/.bashrc
 
-get python3 zsh vim
+$get python3 zsh vim rxvt-unicode
 
+# vim plugins
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+
+cd ~/.vim/bundle
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/altercation/vim-colors-solarized
+
+# SSH keys
+
+# Netsoc first
+ssh-keygen -f ~/.ssh/id_netsoc -N '' -q
+cat ~/.ssh/id_netsoc.pub | ssh netsoc "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys"
